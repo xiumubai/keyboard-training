a94bd9faaa99ba71e4779ba22e6a433651c0553d
xiumubai <1547702880@qq.com>
2022-06-03 18:11:04 +0800

    doc: 添加组合键

---
+++ test.js
@@ -0,0 +1,16 @@
+function binnnarySearch(array, value, compareFn = defaultCompare) {
+  const sortedArray = insertionSort(array);
+  let low = 0;
+  let high = sortedArray.length - 1;
+  while (lesseOrEquals(low, high, compareFn)) {
+    const mid = Math.floor(low + high);
+    const a = 1;
+    const cccc = 333333;
+    const bb = 22;
+    const dddd = 4444;
+  }
+}
+
+function sum(a, b) {
+  return a + b;
+}
